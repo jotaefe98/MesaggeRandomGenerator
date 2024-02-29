7b649913529bc2927d3016342534c9d529e6e08e
jotaefe <juanfranruiz98@gmail.com>
2024-02-29 12:24:29 +0100

    FINAL

---
+++ index.js
@@ -0,0 +1,3 @@
+const phrases = ["The cat sat on the mat.","I went to the store to buy some groceries.","She enjoys reading books in her free time.","They are planning a trip to the beach next weekend.","He cooked a delicious dinner for his family yesterday."]
+const randomNumber = Math.floor(Math.random() * phrases.length);
+console.log(phrases[randomNumber]);
